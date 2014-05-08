7db19dca9cde6114d3b82d6a9ae8a3da16f3d5a0
jujoo <wnfks93@gmail.com>
2014-05-09 01:22:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,3 @@
-/*$(document).ready(function(){
-	console.log("ready");	
-
-	function handleMotionEvent(event) {
-
-	    var x = event.accelerationIncludingGravity.x;
-	    var y = event.accelerationIncludingGravity.y;
-	    var z = event.accelerationIncludingGravity.z;
-
-		$("#xVal").html(x);
-		$("#yVal").html(y);
-		$("#zVal").html(z);
-
-		var maxX = window.innerWidth - $("#ball").width();
-		var maxY = window.innerHeight - $("#ball").height();
-
-		var factor = 3;
-
-		x = Math.round(x * factor);
-
-		var orgX = $("#ball").css("left");
-		orgX = parseFloat(orgX);
-
-		var newX = orgX + x;
-		newX = Math.max(0, newX);
-		newX = Math.min(maxX, newX);
-
-		$("#ball").css("left", Math.round(newX));
-
-		y = Math.round(y * factor);
-
-		var orgY = $("#ball").css("top");
-		orgY = parseFloat(orgY);
-
-		var newY = orgY - y;
-		newY = Math.max(0, newY);
-		newY = Math.min(maxY, newY);
-
-		$("#ball").css("top", Math.round(newY));
-	}
-
-	window.addEventListener("devicemotion", handleMotionEvent, true);
-
-	$("#ball").css("left", 30);
-	$("#ball").css("top", 30);
-}); */
-
 
 
 $(document).ready(function(){
@@ -102,14 +55,16 @@ $(document).ready(function(){
 
 });
 
-var firepoint = function(){
-	if(shakepoint > 150){
-          &('.1").addClass('4');
-        }
-        else if(shakepoint > 100){
-           &('.1").addClass('3');
-        }
-        else if(shakepoint > 50){
-           &('.1").addClass('2');
-        }
-}
+$(document).ready(function(){
+	var firepoint = function(){
+		if(shakepoint > 150){
+	          &('.1").addClass('4');
+	        }
+	        else if(shakepoint > 100){
+	           &('.1").addClass('3');
+	        }
+	        else (shakepoint > 50){
+	           &('.1").addClass('2');
+	        }
+	}
+});
